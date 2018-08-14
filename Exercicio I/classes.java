a491f8ac95b132fb8b85b405d132e4a3bdd19827
Aluno <aluno@cesed.local>
2018-08-13 21:21:17 -0300

    First Commit

---
+++ Exercicio I/classes.java
@@ -0,0 +1,61 @@
+public class AcessaConta {
+	
+	private String mCPF;
+	private List<Conta> mContas;
+	
+	public Conta(List<Conta> mContas){
+		this.mContas = mContas;
+	}
+	
+
+
+	public void getInstanceConta(String cpf){
+		if(mContas != null){
+			for(Conta item : mContas){
+			if(item.getmCPF().equal(cpf)){
+				return item;
+			}
+			
+			
+		} 
+		}else{
+			/** chama serviço e carrega lista de contas **/
+			this.mContas = List<Contas>;
+			getInstanceConta(cpf);
+		}
+		
+	}
+	
+	public boolean deposita(double valor, Conta conta){
+		conta.adcionasaldo(double);
+		/**chamar serviço passando conta **/
+		try{
+			system.outprintln(conta.saldo());
+			return true;
+		}catch{
+			return false;
+		}
+	}
+	
+	
+}
+
+
+public class Conta{
+	private double mSaldo;
+	private String mCPF;
+	
+	public Conta(){
+		
+	}
+	
+	public double getSaldo(){
+		return saldo;
+	}
+	
+	public String getmCPF(){
+		return mCPF;
+	}
+	
+	public 
+}
